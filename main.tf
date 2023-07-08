e1c1e7d3a332f92a09b6b2ca4a700468e76b7f2c
chollamuthunarayanan <chollamuthu.narayanan@unity3d.com>
2023-07-08 21:03:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,13 +56,13 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     vm_size    = "Standard_D2_v2"
     node_count = var.agent_count
   }
-  linux_profile {
-    admin_username = "ubuntu"
+  #linux_profile {
+    #admin_username = "ubuntu"
 
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
-  }
+    #ssh_key {
+    #  key_data = file(var.ssh_public_key)
+    #}
+  #}
   network_profile {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
